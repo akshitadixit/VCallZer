b46f486f0a403b019b9b9de572456b62123e9a68
akshitadixit <akshitadixit.hack@gmail.com>
2021-02-26 18:39:10 +0530

    Integrated dlib with TCP stream

--- client.py
+++ client.py
@@ -1,43 +1,28 @@
-import socket
-import cv2
-import struct
-import pickle
+import socket,cv2, pickle,struct
 
-#creating socket
+# create socket
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-host_ip = '192.168.42.237'
+host_ip = '127.0.0.1' # paste your server ip address here
 port = 9999
-
-#connecting socket
-client_socket.connect((host_ip,port))
-
-#"Q" stands for long unsigned int
+client_socket.connect((host_ip,port)) # a tuple
 data = b""
 payload_size = struct.calcsize("Q")
 while True:
     while len(data) < payload_size:
-	#receive info in packets of size 4*1024
         packet = client_socket.recv(4*1024) # 4K
         if not packet: break
         data+=packet
-
     packed_msg_size = data[:payload_size]
     data = data[payload_size:]
     msg_size = struct.unpack("Q",packed_msg_size)[0]
     
     while len(data) < msg_size:
-	#actual image data from the frame comes here
         data += client_socket.recv(4*1024)
-
     frame_data = data[:msg_size]
     data  = data[msg_size:]
     frame = pickle.loads(frame_data)
-    cv2.imshow("Video receipt",frame)
-
-    #press esc to exit
+    cv2.imshow("RECEIVING VIDEO",frame)
     key = cv2.waitKey(1) & 0xFF
     if key  == ord('q'):
         break
-
 client_socket.close()
--- server.py
+++ server.py
@@ -1,41 +1,80 @@
-import socket
 import cv2
-import pickle
-import struct
+import dlib
+import numpy as np
+import socket, pickle, struct
 
-#creating socket
+# Socket Create
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-#the below is applicable for local host
 host_name  = socket.gethostname()
-host_ip = socket.gethostbyname(host_name)
+#host_ip = socket.gethostbyname(host_name)
+host_ip = '127.0.0.1'
 print('HOST IP:',host_ip)
 port = 9999
 socket_address = (host_ip,port)
 
-#binding socket
+# Socket Bind
 server_socket.bind(socket_address)
 
-#listening to socket [increase value to 15 incase of valueError]
+# Socket Listen
 server_socket.listen(5)
-print("Server port:",socket_address)
+print("LISTENING AT:",socket_address)
 
-#accepting client socket connections
+# Socket Accept
 while True:
     client_socket,addr = server_socket.accept()
-    print('Connected to:',addr)
+    print('GOT CONNECTION FROM:',addr)
+    # Load the detector
+    detector = dlib.get_frontal_face_detector()
+
+    # Load the predictor
+    predictor = dlib.shape_predictor("C://Users//Gaurav//Downloads//shape_predictor_81_face_landmarks.dat")
+
     if client_socket:
-        vid = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture(0)
+        i=0
         
-        while(vid.isOpened()):
-            img,frame = vid.read()
-            a = pickle.dumps(frame)
+        while(cap.isOpened()):
+            _, frame = cap.read()
+            #choose any path u want to here
+            path = 'C:\\imgs\\img'+str(i)+'.jpg'
+            cv2.imwrite(path,frame)
+            i = i+1
+            # Convert image into grayscale
+            gray = cv2.cvtColor(src=frame, code=cv2.COLOR_BGR2GRAY)
+        
+            black = np.zeros((gray.shape[0],gray.shape[1],3), np.uint8)
+        
+            # Use detector to find landmarks
+            faces = detector(gray)
+        
+            for face in faces:
+                x1 = face.left()  # left point
+                y1 = face.top()  # top point
+                x2 = face.right()  # right point
+                y2 = face.bottom()  # bottom point
+        
+                # Create landmark object
+                landmarks = predictor(image=gray, box=face)
+        
+        
+                # Loop through all the points
+                for n in range(0, 81):
+                    x = landmarks.part(n).x
+                    y = landmarks.part(n).y
+        
+                    # Draw a circle
+                    cv2.circle(img=black, center=(x, y), radius=1, color=(0, 255,0),thickness=-1)
+            
+            a = pickle.dumps(black)
             message = struct.pack("Q",len(a))+a
             client_socket.sendall(message)
             
-            cv2.imshow('Video transmission',frame)
-
-	    #press esc to exit
+            cv2.imshow('TRANSMITTING VIDEO',black)
             key = cv2.waitKey(1) & 0xFF
             if key ==ord('q'):
                 client_socket.close()
+                break # When everything done, release the video capture and video write objects
+    cap.release() #inside if
+                
+                # Close all windows
+    cv2.destroyAllWindows() #inside if
