21793ca300889aa0ef2a7993a8e3ad157becc574
Sidhved <66831453+Sidhved@users.noreply.github.com>
2021-06-13 11:15:47 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
 
+# Initialize tkinter token with the root widget
 root = Tk()
 style = ttk.Style(root)
 #adjust the path here according to your local env
@@ -10,6 +11,7 @@ root.call('package', 'require', 'awdark')
 current_theme = style.theme_use('awdark')
 root.iconphoto(False, PhotoImage(file="icon.ico"))
 
+# Configuration Specifications
 label = Label(root, width=768)
 #adjust the path here according to your locak env
 img = Image.open(r"C:\\Users\\miniconda3\\envs\\test\\sample.png")
@@ -23,11 +25,13 @@ label.rowconfigure((1,2,3), weight=1)
 label.columnconfigure((0,4), weight=0)
 label.columnconfigure((1,2), weight=1)
 
+# Calling Label Widget with the specified IP
 ttk.Label(label, text="Your IP: ").grid(row = 1, column = 1, sticky='se', padx=20)
 e1 = ttk.Entry(label)
 e1.place(anchor=CENTER)
 e1.grid(row = 1, column = 2, sticky='sw')
 
+# Initializing Radiobutton Widget
 R1 = ttk.Radiobutton(label, text="Option 1", variable=IntVar(), value=1)
 R2 = ttk.Radiobutton(label, text="Option 2", variable=IntVar(), value=2)
 R1.place(relx=0.5, rely=0.5, anchor=CENTER)
@@ -41,4 +45,4 @@ root.minsize(768,600)
 root.maxsize(768,600)
 root.title("VCallZer")
 
-root.mainloop()
+root.mainloop() # root widget call
